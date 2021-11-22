9be16bb755e91853d23ba7e61f14b960963e8d2c
Khoi Pham <pckhoi@gmail.com>
2021-11-22 07:22:24 +0700

    doc: update doc config

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Wrgl Python SDK'
-copyright = '2021, Khoi Pham'
+copyright = '2021, Wrangle Ltd'
 author = 'Khoi Pham'
 
 # The full version, including alpha/beta/rc tags
